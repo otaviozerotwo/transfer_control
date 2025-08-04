72055428a0c2c619e2340e517c9814eabc02db4c
Otavio <otavio.zerotwo@gmail.com>
2025-08-03 22:26:26 -0300

    fix: add trim() authSchema melhoria form LoginScreen

--- t_control_server/src/schemas/authSchema.ts
+++ t_control_server/src/schemas/authSchema.ts
@@ -1,8 +1,8 @@
 import * as z from 'zod/v4';
 
 export const authBodySchema = z.object({
-  username: z.string().min(1, 'username é obrigatório'),
-  password: z.string().min(1, 'password é obrigatório.'),
+  username: z.string().trim().min(1, 'username é obrigatório'),
+  password: z.string().trim().min(1, 'password é obrigatório.'),
 });
 
 export type AuthBodyDTO = z.infer<typeof authBodySchema>;
